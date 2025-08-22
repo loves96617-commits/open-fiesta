07ec07e399dd339782763a958159cbfb803b17f9
Lokeswaran Aruljothy <74011196+lokeswaran-aj@users.noreply.github.com>
2025-08-22 19:27:07 +0530

    chore: Add Vercel Analytics and Speed Insights to the layout (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -39,6 +41,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
