ae4940a45d7609afd8d8a909dd8f30e53add2ad9
Lokeswaran Aruljothy <74011196+lokeswaran-aj@users.noreply.github.com>
2025-08-23 20:23:00 +0530

    feat: Integrate OpenTelemetry with Langsmith for enhanced telemetry and monitoring (#4)

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,8 @@
 import { convertToModelMessages, streamText, type UIMessage } from "ai";
+import { initializeOTEL } from "langsmith/experimental/otel/setup";
 
 export const maxDuration = 60;
+initializeOTEL();
 
 export async function POST(req: Request) {
   const { messages, model }: { messages: UIMessage[]; model: string } =
@@ -11,6 +13,13 @@ export async function POST(req: Request) {
     messages: convertToModelMessages(messages),
     system:
       "You are a friendly assistant! Keep your responses concise and helpful.",
+    experimental_telemetry: {
+      isEnabled: true,
+      metadata: {
+        ls_run_name: `open-fiesta`,
+        environment: process.env.NODE_ENV,
+      },
+    },
   });
 
   return result.toUIMessageStreamResponse({
